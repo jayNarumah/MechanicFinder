2966cee7b3bd2bdac9370af69fd35c2c5d99978c
Joseph Onyilo <onyilo@gmail.com>
2022-08-11 13:52:30 +0100

    PDF download initiated

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class NotificationController extends Controller
 {
-    //
+    function printPDF($name, $view)
+    {
+        $pdf = Pdf::loadView('pdf.invoice');
+        return $pdf->download($name + '.pdf');
+    }
+
+    function termsCondtions()
+    {
+        $this->printPDF('terms-and-conditions', 'terms-and-conditions');
+    }
 }
